9db6c4aa0c8ba62f75421c89aeb37cc3ee7937ee
Black Ghost <balckghostgithub@gmail.com>
2024-04-02 18:07:53 +0200

    Din test

--- main.c
+++ main.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
 
-
-
-void generate_num_4(){
-  FILE *file_num_4 = fopen("numbers_4.txt", "w");
-  if (file_num_4 == NULL) {
-      printf("Error\n");
+void generate_password(int length, const char *filename) {
+  FILE *file = fopen(filename, "w");
+  if (file == NULL) {
+      printf("Error opening file.\n");
+      return;
   }
 
-  int i, j, k, l;
-  for (i = 0; i <= 9; i++) {
-      for (j = 0; j <= 9; j++) {
-          for (k = 0; k <= 9; k++) {
-              for (l = 0; l <= 9; l++) {
-                  fprintf(file_num_4, "%d%d%d%d\n", i, j, k, l);
-              }
-          }
-      }
+  int max_value = 1;
+  for (int i = 0; i < length; i++) {
+      max_value *= 10;
   }
 
-  fclose(file_num_4);
+  for (int i = 0; i < max_value; i++) {
+      fprintf(file, "%0*d\n", length, i);
+  }
+
+  fclose(file);
 }
+
+  void generate_num_4(){
+    FILE *file_num_4 = fopen("numbers_4.txt", "w");
+    if (file_num_4 == NULL) {
+        printf("Error\n");
+    }
+
+    int i, j, k, l;
+    for (i = 0; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
+            for (k = 0; k <= 9; k++) {
+                for (l = 0; l <= 9; l++) {
+                    fprintf(file_num_4, "%d%d%d%d\n", i, j, k, l);
+                }
+            }
+        }
+    }
+
+    fclose(file_num_4);
+  }
 
 void generate_num_6(){
   FILE *file_num_6 = fopen("numbers_6.txt", "w");
@@ -49,6 +66,9 @@ void generate_num_6(){
 
 
 int main() {
+
+    //generate_password(8, "test.txt");
+
     printf("==== Password generator ====\n\n");
     printf("1 -> Generate 4 digital passwords\n");
     printf("2 -> Generate 6 digital passwords\n");
